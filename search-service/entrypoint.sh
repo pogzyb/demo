556962b4432c2a2fb919369d1d483bb2a585096a
pogzyb <pogzyb@umich.edu>
2021-09-03 10:23:13 -0500

    Add entrypoint script

---
+++ search-service/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# `entrypoint.sh` will start the application.
+# If running in debug mode, the werkzeug development server is started via the flask cli.
+# Otherwise, gunicorn is used to serve the application in production mode.
+
+debug=${FLASK_DEBUG}
+if [[ $debug == "0" ]]; then
+  echo "Running in production mode."
+  gunicorn \
+    --workers 1 \
+    --threads 4 \
+    --bind 0.0.0.0:${PORT} \
+    "app:create_app('production')"
+else
+  echo "Running in non-production mode."
+  flask run --host=0.0.0.0 --port=${PORT}
+fi
